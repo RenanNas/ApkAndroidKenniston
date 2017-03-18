f60279ecb2a6a74df40800a51e93c85914f1007b
RenanNas <santintobi@gmail.com>
2017-03-17 21:42:13 -0300

    mapas

---
+++ app/src/main/java/br/iesb/contatos/MapActivity.java
@@ -0,0 +1,33 @@
+package br.iesb.contatos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+private GoogleMap mMap;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mMap = googleMap;
+        LatLng iesbSul = new LatLng(-15.8346329, -47.9130827);
+        mMap.addMarker(new MarkerOptions().position(iesbSul).title("DOGAO DO IESB"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(iesbSul, 15));
+
+    }
+}
