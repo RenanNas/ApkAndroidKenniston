d8dc239351a5056ba229b6369c38b4aad869c56b
RenanNas <santintobi@gmail.com>
2017-03-23 21:42:20 -0300

    mapas part 2

--- app/src/main/java/br/iesb/contatos/MapActivity.java
+++ app/src/main/java/br/iesb/contatos/MapActivity.java
@@ -1,5 +1,12 @@
 package br.iesb.contatos;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,7 +18,10 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
-private GoogleMap mMap;
+    public static final int MAP_PERMISSION_ACCESS_COURSE_LOCATION = 9999;
+
+    private GoogleMap mMap;
+    LocationManager locationManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,9 +35,72 @@ private GoogleMap mMap;
     public void onMapReady(GoogleMap googleMap) {
 
         mMap = googleMap;
+
+
+
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
+            ActivityCompat.requestPermissions( this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION}, MAP_PERMISSION_ACCESS_COURSE_LOCATION);
+        } else {
+            getLastLocation();
+            getLocation();
+        }
         LatLng iesbSul = new LatLng(-15.8346329, -47.9130827);
         mMap.addMarker(new MarkerOptions().position(iesbSul).title("DOGAO DO IESB"));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(iesbSul, 15));
+    }
 
+    private void getLastLocation() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED ) {
+            Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
+            if( lastKnownLocation != null ) {
+                LatLng me = new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
+                mMap.addMarker(new MarkerOptions().position(me).title("Eu estava aqui quando o anrdoid me localizou pela última vez!!!"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(me, 10));
+
+            }
+        }
+    }
+
+
+    private void getLocation() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED ) {
+            LocationListener locationListener = new LocationListener() {
+                public void onLocationChanged(Location location) {
+                    LatLng me = new LatLng(location.getLatitude(), location.getLongitude());
+                    mMap.addMarker(new MarkerOptions().position(me).title("Estou Aqui!!!"));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(me, 10));
+                }
+
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+                }
+
+                public void onProviderEnabled(String provider) {
+                }
+
+                public void onProviderDisabled(String provider) {
+                }
+            };
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MAP_PERMISSION_ACCESS_COURSE_LOCATION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getLastLocation();
+                    getLocation();
+                } else {
+                    //Permissão negada
+                }
+                return;
+            }
+        }
     }
 }
+
